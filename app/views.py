a2c63154e01ae903ce9e1c44bccd7182acc25ba9
Elizabeth <proyecto.elizabethmargott@gmail.com>
2023-12-18 09:58:47 -0500

    Urls, views

--- app/views.py
+++ app/views.py
@@ -4,6 +4,74 @@ from .models import *
 from .forms import *
 from django.shortcuts import render, redirect, get_object_or_404
 
-def home(req):
-    context = {}
-    return render(req, 'home.html', context)
+def listar_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'listar_libros.html', {'libros': libros})
+
+def crear_libros(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm()
+    return render(request, 'crear_libros.html', {'form': form})
+
+def detalles_libros(request):
+    libro = get_object_or_404(Libro, pk=pk)
+    return render(request, 'detalles_libros.html', {'libro': libro})
+
+def editar_libros(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    if request.method == 'POST':
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm(instance=libro)
+    return render(request, 'editar_libros.html', {'form': form})
+
+def eliminar_libros(request, pk):
+    libro = get_object_or_404(Libro, pk=pk)
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('listar_libros')
+    return render(request, 'eliminar_libros.html', {'libro': libro})
+
+def listar_autores(request):
+    autores = Autores.objects.all()
+    return render(request, 'listar_autores.html', {'autores': autores})
+
+def crear_autores(request):
+    if request.method == 'POST':
+        form = AutoresForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_autores')
+    else:
+        form = AutoresForm()
+    return render(request, 'crear_autores.html', {'form': form})
+
+def detalles_autores(request):
+    autor = get_object_or_404(Autores, pk=pk)
+    return render(request, 'detalles_autores.html', {'autor': autor})
+
+def editar_autores(request, pk):
+    autor = get_object_or_404(Autores, pk=pk)
+    if request.method == 'POST':
+        form = AutoresForm(request.POST, instance=autor)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_autores')
+    else:
+        form = AutoresForm(instance=autor)
+    return render(request, 'editar_autores.html', {'form': form})
+
+def eliminar_autores(request, pk):
+    autor = get_object_or_404(Autores, pk=pk)
+    if request.method == 'POST':
+        autor.delete()
+        return redirect('listar_autores')
+    return render(request, 'eliminar_autores.html', {'autor': autor})
