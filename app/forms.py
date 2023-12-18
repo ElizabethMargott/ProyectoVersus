f24601287da1f057e5c1701285e44cef53dc2f91
Elizabeth <proyecto.elizabethmargott@gmail.com>
2023-12-18 11:59:07 -0500

    styles

--- app/forms.py
+++ app/forms.py
@@ -1,27 +1,53 @@
 from django import forms
 from .models import *
 
-class  AutorForm(forms.ModelForm):
+class AutorForm(forms.ModelForm):
     class Meta:
         model = Autor
-        fields = '__all__' 
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'nacionalidad': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
-class  EditorialForm(forms.ModelForm):
+class EditorialForm(forms.ModelForm):
     class Meta:
         model = Editorial
-        fields = '__all__' 
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
-class  LibroForm(forms.ModelForm):
+class LibroForm(forms.ModelForm):
     class Meta:
         model = Libro
-        fields = '__all__' 
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'paginas': forms.NumberInput(attrs={'class': 'form-control'}),
+            'editorial': forms.Select(attrs={'class': 'form-control'}),
+            'autor': forms.Select(attrs={'class': 'form-control'}),
+            'imagen': forms.FileInput(attrs={'class': 'form-control'}),
+        }
 
-class  EstudianteForm(forms.ModelForm):
+class EstudianteForm(forms.ModelForm):
     class Meta:
         model = Estudiante
-        fields = '__all__' 
+        fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellido': forms.TextInput(attrs={'class': 'form-control'}),
+            'codigo_estudiante': forms.TextInput(attrs={'class': 'form-control'}),
+        }
 
-class  PrestamoForm(forms.ModelForm):
+class PrestamoForm(forms.ModelForm):
     class Meta:
         model = Prestamo
-        fields = '__all__' 
+        fields = '__all__'
+        widgets = {
+            'libro': forms.Select(attrs={'class': 'form-control'}),
+            'estudiante': forms.Select(attrs={'class': 'form-control'}),
+            'fecha_prestamo': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'fecha_devolucion': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def listar_libros(request):
 
 def crear_libros(request):
     if request.method == 'POST':
-        form = LibroForm(request.POST)
+        form = LibroForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             return redirect('listar_libros')
